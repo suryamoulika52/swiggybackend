d434466edb9eb1f8aab95ac389aed21a90817563
suryamoulika52 <suryamoulika23@gmail.com>
2025-02-05 11:36:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express=require('express');
-const app=express();
+
 const dotEnv=require('dotenv');
 const mongoose=require('mongoose');
 const vendorRoutes=require('./routes/vendorRoutes');
 const firmRoutes=require('./routes/firmRoutes')
 const productRoutes=require('./routes/productRoutes')
 const bodyParser=require('body-parser');
-const path=require('path');
+
 const cors=require('cors');
+const path=require('path');
+const app=express();
 const PORT=process.env.PORT||4000;
 dotEnv.config();
+app.use(cors())
 mongoose.connect(process.env.MONGO_URI)
 .then(()=>{
     console.log("Database connected successfully");
@@ -22,7 +25,6 @@ mongoose.connect(process.env.MONGO_URI)
 app.use(bodyParser.json());
 //the data from registered vendor in database
 //the vendor/register is url because the vendorRoutes has data from post with url '/register'
-app.use(cors());
 app.use('/vendor',vendorRoutes);
 app.use('/firm',firmRoutes);
 app.use('/product',productRoutes);
